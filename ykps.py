abde154de8068c1c5e9a9190221826a79bee1475
Runxi Yu <me@runxiyu.org>
2024-05-17 16:41:18 +0800

    Add primitive sjdb/submit page

--- ykps.py
+++ ykps.py
@@ -13,6 +13,8 @@ from flask.wrappers import Response
 import time, os
 app = Flask(__name__)
 
+MAX_FILE_SIZE = 3000000  # only used in client-side validation
+MAX_REQUEST_SIZE = 3145728  # only used in server-side validation
 VERSION = """ykps-wsgi v0.1
 
 License: GNU Affero General Public License v3.0 only
@@ -23,16 +25,20 @@ def index():
     return render_template("index.html")
 
 @app.route('/version', methods=['GET'])
-def index():
+def version():
     return Response(__VERSION__, mimetype="text/plain")
 
 @app.route('/sjdb/', methods=['GET'])
 def sjdb_index():
     return render_template("sjdb-about.html")
 
-@app.route('/sjdb/ack', methods=['GET'])
+@app.route('/sjdb/acks', methods=['GET'])
 def sjdb_ack():
-    return render_template("sjdb-ack.html")
+    return render_template("sjdb-acks.html")
+
+@app.route('/sjdb/submit', methods=['GET'])
+def sjdb_submit():
+    return render_template("sjdb-submit.html", max_request_size = MAX_REQUEST_SIZE, max_file_size = MAX_FILE_SIZE)
 
 if __name__ == "__main__":
-    app.run(port=8080)
+    app.run(port=8080, debug=True)
