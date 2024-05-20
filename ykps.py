5a880d854c118dac95162aeb1055cb898a2042a2
Runxi Yu <me@runxiyu.org>
2024-05-20 21:52:28 +0800

    Add basic authentication for sjdb-submit; add error page

--- ykps.py
+++ ykps.py
@@ -12,12 +12,18 @@ import typing
 import time
 import os
 import json
+import sys
+import traceback
+
+# import configparser
 
 import flask
 import werkzeug
-import msal  # type: ignore
+import identity.flask  # type: ignore
 
 response_t: typing.TypeAlias = typing.Union[werkzeug.Response, flask.Response, str]
+
+NOT_IMPLEMENTED = 501
 
 MAX_FILE_SIZE = 3000000  # only used in JS client-side validation
 MAX_REQUEST_SIZE = 3145728  # only used in server-side validation
@@ -27,8 +33,37 @@ License: GNU Affero General Public License v3.0 only
 URLs: https://git.runxiyu.org/ykps/current/ykps-wsgi.git
       https://git.sr.ht/~runxiyu/ykps-wsgi.git"""
 
+AUTHORITY = "https://login.microsoftonline.com/ddd3d26c-b197-4d00-a32d-1ffd84c0c295"
+CLIENT_ID = "651eef7e-8670-4b68-b7ed-d2d7885187e4"
+SCOPE = ["https://graph.microsoft.com/.default"]
+REDIRECT_URL = "http://localhost:8080/auth"
+
+for fn in ["/srv/ykps/secret.txt", "secret.txt"]:
+    try:
+        with open(fn, "r") as fd:
+            CLIENT_SECRET = fd.read().strip("\n")
+        break
+    except FileNotFoundError:
+        pass
+else:
+    raise FileNotFoundError("secret.txt")
+# with open("thumb.txt", "r") as fd:
+#     THUMBPRINT = fd.read().strip("\n")
+# with open("server.pem", "r") as fd:
+#     PRIVATE_KEY = fd.read()
+
 app = flask.Flask(__name__)
 app.config["MAX_CONTENT_LENGTH"] = MAX_REQUEST_SIZE
+app.config["SESSION_TYPE"] = "filesystem"
+
+
+auth = identity.flask.Auth(
+    app,
+    CLIENT_ID,
+    client_credential=CLIENT_SECRET,
+    redirect_uri=REDIRECT_URL,
+    authority=AUTHORITY,
+)
 
 
 @app.route("/", methods=["GET"])
@@ -39,6 +74,36 @@ def index() -> response_t:
 @app.route("/version", methods=["GET"])
 def version() -> response_t:
     return flask.Response(VERSION, mimetype="text/plain")
+
+
+@app.route("/uinfo", methods=["GET"])
+@auth.login_required  # type: ignore
+def uinfo(context) -> response_t:
+    return flask.Response(json.dumps(context), mimetype="application/json")
+
+
+# @app.route("/login", methods=["GET"])
+# def login() -> response_t:
+#     return flask.Response("", mimetype="text/plain")
+
+# @app.route("/auth", methods=["GET", "POST"])
+# def auth() -> response_t:
+#     return flask.Response("", mimetype="text/plain")
+
+
+@app.route("/error", methods=["GET"])
+def error() -> response_t:
+    try:
+        raise Exception("THIS IS ONLY A TEST.")
+    except Exception:
+        return flask.Response(
+            flask.render_template(
+                "oops.html",
+                msg="THIS IS NOT A REAL ERROR. You found the endpoint that is used to test the error page. Please do NOT submit a bug report about this.",
+                error=traceback.format_exc(),
+            ),
+            status=500,
+        )
 
 
 @app.route("/sjdb/", methods=["GET"])
@@ -57,17 +122,31 @@ def sjdb_ack() -> response_t:
 
 
 @app.route("/sjdb/submit", methods=["GET", "POST"])
-def sjdb_submit() -> response_t:
+@auth.login_required  # type: ignore
+def sjdb_submit(context) -> response_t:
+    try:
+        display_name = context["user"]["name"]
+    except KeyError:
+        return flask.Response(
+            flask.render_template(
+                "oops.html",
+                subproject="sjdb",
+                msg="I cannot obtain your display name. There might be an issue while you were logging in.",
+                error=traceback.format_exc(),
+            ),
+            status=500,
+        )
     if flask.request.method == "GET":
         return flask.render_template(
             "sjdb-submit.html",
             max_request_size=MAX_REQUEST_SIZE,
             max_file_size=MAX_FILE_SIZE,
+            display_name=display_name,
         )
     elif flask.request.method == "POST":
         # NOTE: Do not place duplicate keys in the form! The conversion will only yield the first one.
         jd = json.dumps(flask.request.form.to_dict())
-        return flask.Response(jd, mimetype="text/plain")
+        return flask.Response(jd, mimetype="text/plain", status=NOT_IMPLEMENTED)
         # return flask.render_template("sjdb-submit-post.html")
     else:
         return flask.Response(
